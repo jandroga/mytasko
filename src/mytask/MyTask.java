12cf11cdd3e056529b57dfd6c25bbdb863acd7e8
jandroga <71236300+jandroga@users.noreply.github.com>
2021-11-03 09:38:15 +0100

    Primer commit

---
+++ src/mytask/MyTask.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mytask;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author bende
+ */
+public class MyTask extends JFrame {
+    
+    private Chef ch = new Chef();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        // TODO code application logic here
+        
+    }
+    
+}
